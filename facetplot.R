cf4f99873d36871e94dbaae22749fcee236205c6
DianaOaxaca <hoaxacadiana@gmail.com>
2021-08-10 15:52:26 -0500

    add directory

--- facetplot.R
+++ facetplot.R
@@ -5,5 +5,7 @@ library(dplyr)
 library(ggplot2)
 
 #Crear facet plot
+png("mis_imagenes/facetplot.png")
 my_scatplot <- ggplot(mtcars,aes(x=wt,y=mpg,col=cyl)) + geom_point()
 my_scatplot + facet_grid(~cyl)
+dev.off()
